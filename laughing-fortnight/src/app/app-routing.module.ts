b6476aded6f05d61772420308035641df1448e8b
juninhoww2 <alqadianokira@gmail.com>
2020-12-23 00:32:42 -0300

    finishloginandsignup

--- laughing-fortnight/src/app/app-routing.module.ts
+++ laughing-fortnight/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -26,6 +26,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
   },
 ];
 
